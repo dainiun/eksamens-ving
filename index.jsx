1ab2b64d13b5ce25e27b863d831ac7d9e89032b9
dainiun <69790820+dainiun@users.noreply.github.com>
2022-04-27 16:33:56 +0200

    Navigering
    Navigering

--- index.jsx
+++ index.jsx
@@ -1,7 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+
+function FrontPage() {
+    return <div>
+        <h1>Database av et slag</h1>
+        <ul>
+            <li><Link to="/product/new">Nytt produkt</Link></li>
+            <li><Link to="/products">Alle produkter</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+        return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage />}/>
+            <Route path="/product/new" element={<h1> Nytt produkt </h1>}/>
+            <Route path="/products" element={<h1> Liste over produkt </h1>}/>
+        </Routes>;
+    </BrowserRouter>;
+}
+
+
 
 ReactDOM.render(
-    <h1>Database av et slag</h1>,
+    <Application/>,
     document.getElementById("app")
 );
